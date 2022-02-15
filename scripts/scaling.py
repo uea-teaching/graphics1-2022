49e13d9a0105d3a62c449d18eeae5b9a28b0f93b
Dave Greenwood <dgrnwd@gmail.com>
2022-02-15 15:46:11 +0000

    labels

--- scripts/scaling.py
+++ scripts/scaling.py
@@ -30,8 +30,7 @@ t1 = (s1 @ t0.T).T
 
 fig, ax = plt.subplots(1, 1)
 set_spines(ax, x_range=(-12, 12), y_range=(-8, 8))
-plot_triangle(ax, t0, label_offsets=t0_offset,
-              facecolor=(0.5, 0.0, 0.5, 0.2))
+plot_triangle(ax, t0, labels=False, facecolor=(0.5, 0.0, 0.5, 0.2))
 plot_triangle(ax, t1, label_offsets=t0_offset)
 fig.savefig("scaling_02.png")
 
@@ -42,8 +41,7 @@ t2 = (s2 @ t0.T).T
 
 fig, ax = plt.subplots(1, 1)
 set_spines(ax, x_range=(-12, 12), y_range=(-8, 8))
-plot_triangle(ax, t0, label_offsets=t0_offset,
-              facecolor=(0.5, 0.0, 0.5, 0.2))
+plot_triangle(ax, t0, labels=False, facecolor=(0.5, 0.0, 0.5, 0.2))
 plot_triangle(ax, t2, label_offsets=t0_offset)
 fig.savefig("scaling_03.png")
 
@@ -54,9 +52,9 @@ t3 = (s3 @ t0.T).T
 
 fig, ax = plt.subplots(1, 1)
 set_spines(ax, x_range=(-12, 12), y_range=(-8, 8))
-plot_triangle(ax, t0, label_offsets=t0_offset,
-              facecolor=(0.5, 0.0, 0.5, 0.2))
-plot_triangle(ax, t3, label_offsets=t0_offset)
+plot_triangle(ax, t0, labels=False, facecolor=(0.5, 0.0, 0.5, 0.2))
+plot_triangle(ax, t3, label_offsets=[
+    [-0.5, -0.75], [0.0, 0.0], [-0.5, -0.75]])
 fig.savefig("shear_x.png")
 
 # %%
@@ -66,7 +64,42 @@ t4 = (s4 @ t0.T).T
 
 fig, ax = plt.subplots(1, 1)
 set_spines(ax, x_range=(-12, 12), y_range=(-8, 8))
-plot_triangle(ax, t0, label_offsets=t0_offset,
-              facecolor=(0.5, 0.0, 0.5, 0.2))
+plot_triangle(ax, t0, labels=False, facecolor=(0.5, 0.0, 0.5, 0.2))
 plot_triangle(ax, t4, label_offsets=t0_offset)
 fig.savefig("shear_y.png")
+
+# %%
+
+s5 = np.array([[-1, 0], [0, 1]])
+t5 = (s5 @ t0.T).T
+
+fig, ax = plt.subplots(1, 1)
+set_spines(ax, x_range=(-12, 12), y_range=(-8, 8))
+plot_triangle(ax, t0, labels=False, facecolor=(0.5, 0.0, 0.5, 0.2))
+plot_triangle(ax, t5, label_offsets=t0_offset)
+fig.savefig("reflect_x.png")
+
+# %%
+
+s6 = np.array([[1, 0], [0, -1]])
+t6 = (s6 @ t0.T).T
+
+fig, ax = plt.subplots(1, 1)
+set_spines(ax, x_range=(-12, 12), y_range=(-8, 8))
+plot_triangle(ax, t0, labels=False, facecolor=(0.5, 0.0, 0.5, 0.2))
+plot_triangle(ax, t6, label_offsets=[
+    [-0.5, 0.25], [-2.25, 0.25], [0, -0.75]])
+fig.savefig("reflect_y.png")
+
+# %%
+
+s7 = np.array([[0, 1], [1, 0]])
+t7 = (s7 @ t0.T).T
+
+fig, ax = plt.subplots(1, 1)
+set_spines(ax, x_range=(-12, 12), y_range=(-8, 8))
+plot_triangle(ax, t0, labels=False, facecolor=(0.5, 0.0, 0.5, 0.2))
+plot_triangle(ax, t7, label_offsets=[
+    [-0.5, -0.75], [-1.75, 0.25], [0.5, -0.75]])
+ax.plot([-10, 10], [-10, 10], 'k--', lw=1)
+fig.savefig("reflect_xy.png")
