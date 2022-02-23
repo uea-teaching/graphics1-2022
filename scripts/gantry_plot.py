1a689c776fbe7d83065b58be93a1941621cbb3b3
Dave Greenwood <dgrnwd@gmail.com>
2022-02-23 16:24:24 +0000

    refactor

--- scripts/gantry_plot.py
+++ scripts/gantry_plot.py
@@ -14,6 +14,9 @@ from utils import *
 set_style()
 plt.rcParams['figure.figsize'] = [6.0, 5.0]
 
+spines = dict(x_range=(-1, 6), y_range=(-1, 6),
+              colour=(0.5, 0.5, 0.5), line_width=0.7)
+
 FILL0 = (0.5, 0.5, 0.5, 0.5)
 FILL1 = (0.5, 0.0, 0.5, 0.5)
 FILL2 = (0.0, 0.5, 0.5, 0.5)
@@ -21,33 +24,9 @@ FILL3 = (0.5, 0.5, 0.0, 0.5)
 STROKE = (0.0, 0.0, 0.0, 1.0)
 
 
-# %%
-
-# square homogeneous coordinates
-square = np.array([
-    [-0.5, -0.5, 1], [0.5, -0.5, 1],
-    [0.5, 0.5, 1], [-0.5, 0.5, 1]]).T
-
-# %%
-
-spines = dict(x_range=(-1, 6), y_range=(-1, 6),
-              colour=(0.5, 0.5, 0.5), line_width=0.7)
-
-
 def plot_rect(ax, rect, col=FILL0):
     """plot a rectangle"""
     ax.fill(rect[0], rect[1], facecolor=col, edgecolor=STROKE, linewidth=1)
-
-
-# start
-fig, ax = plt.subplots(1, 1)
-set_spines(ax, **spines)
-ax.text(0, 0.7, s=r'$s$', ha='center', va='center')
-plot_rect(ax, square)
-ax.grid(False)
-fig.savefig("articulated_start.png")
-
-# %%
 
 
 def scale_xy(x=1, y=1):
@@ -71,23 +50,19 @@ def R(a):
                      [np.sin(rad), np.cos(rad), 0],
                      [0, 0, 1]])
 
-
 # %%
 
+
+# DATA
 a, b, c, alpha, beta = 2, 3, 2, 30, 45
 
-# %%
+# square homogeneous coordinates
+square = np.array([
+    [-0.5, -0.5, 1], [0.5, -0.5, 1],
+    [0.5, 0.5, 1], [-0.5, 0.5, 1]]).T
 
 # gantry
 gantry = trans_xy(y=a/2) @ scale_xy(y=a) @ square.copy()
-
-fig, ax = plt.subplots(1, 1)
-set_spines(ax, **spines)
-plot_rect(ax, gantry, col=FILL1)
-ax.grid(False)
-
-
-# %%
 
 # link1
 link1 = (trans_xy(y=a)
@@ -95,14 +70,6 @@ link1 = (trans_xy(y=a)
          @ trans_xy(x=b / 2)
          @ scale_xy(x=b)
          @ square.copy())
-
-fig, ax = plt.subplots(1, 1)
-set_spines(ax, **spines)
-plot_rect(ax, gantry, col=FILL1)
-plot_rect(ax, link1, col=FILL2)
-ax.grid(False)
-
-# %%
 
 # link2
 link2 = (trans_xy(y=a)
@@ -113,28 +80,17 @@ link2 = (trans_xy(y=a)
          @ scale_xy(x=c)
          @ square.copy())
 
-fig, ax = plt.subplots(1, 1)
-set_spines(ax, **spines)
-plot_rect(ax, gantry, col=FILL1)
-plot_rect(ax, link1, col=FILL2)
-plot_rect(ax, link2, col=FILL3)
-ax.grid(False)
-
 # %%
 
+# start
 fig, ax = plt.subplots(1, 1)
 set_spines(ax, **spines)
-plot_rect(ax, gantry, col=FILL1)
-plot_rect(ax, link1, col=FILL2)
-plot_rect(ax, link2, col=FILL3)
+ax.text(0, 0.7, s=r'$s$', ha='center', va='center')
+plot_rect(ax, square)
 ax.grid(False)
-ax.text(-0.75, 1, s=rf'${a}s$', ha='center', va='center', fontsize=12)
-ax.text(1, 3.5, s=rf'${b}s$', ha='center', va='center', fontsize=12)
-ax.text(2, 4.75, s=rf'${c}s$', ha='center', va='center', fontsize=12)
+fig.savefig("articulated_start.png")
 
-
-# %%
-
+# end
 p1_1 = (link1[:2, 0] + link1[:2, 3]) * 0.5
 p1_2 = p1_1 + 2 * (link1[:2, 1] - link1[:2, 0]).T
 
@@ -164,4 +120,30 @@ AngleAnnotation(
     textposition='outside',
     color='k', text=rf"$\beta={beta}^{{\circ}}$")
 
+fig.savefig("articulated_final.png")
+
+
 # %%
+
+fig, ax = plt.subplots(1, 1)
+set_spines(ax, **spines)
+plot_rect(ax, gantry, col=FILL1)
+ax.grid(False)
+
+
+# %%
+
+fig, ax = plt.subplots(1, 1)
+set_spines(ax, **spines)
+plot_rect(ax, gantry, col=FILL1)
+plot_rect(ax, link1, col=FILL2)
+ax.grid(False)
+
+# %%
+
+fig, ax = plt.subplots(1, 1)
+set_spines(ax, **spines)
+plot_rect(ax, gantry, col=FILL1)
+plot_rect(ax, link1, col=FILL2)
+plot_rect(ax, link2, col=FILL3)
+ax.grid(False)
