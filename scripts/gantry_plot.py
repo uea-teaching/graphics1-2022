807750f292a06c5169c45a6563ea5f3ea8fb4a12
Dave Greenwood <dgrnwd@gmail.com>
2022-02-23 16:18:26 +0000

    annotation

--- scripts/gantry_plot.py
+++ scripts/gantry_plot.py
@@ -14,6 +14,7 @@ from utils import *
 set_style()
 plt.rcParams['figure.figsize'] = [6.0, 5.0]
 
+FILL0 = (0.5, 0.5, 0.5, 0.5)
 FILL1 = (0.5, 0.0, 0.5, 0.5)
 FILL2 = (0.0, 0.5, 0.5, 0.5)
 FILL3 = (0.5, 0.5, 0.0, 0.5)
@@ -26,25 +27,28 @@ STROKE = (0.0, 0.0, 0.0, 1.0)
 square = np.array([
     [-0.5, -0.5, 1], [0.5, -0.5, 1],
     [0.5, 0.5, 1], [-0.5, 0.5, 1]]).T
-square.shape
 
 # %%
 
+spines = dict(x_range=(-1, 6), y_range=(-1, 6),
+              colour=(0.5, 0.5, 0.5), line_width=0.7)
 
-def plot_rect(ax, rect, col=FILL1):
+
+def plot_rect(ax, rect, col=FILL0):
     """plot a rectangle"""
     ax.fill(rect[0], rect[1], facecolor=col, edgecolor=STROKE, linewidth=1)
 
 
 # start
 fig, ax = plt.subplots(1, 1)
-set_spines(ax, x_range=(-1, 6), y_range=(-1, 6))
+set_spines(ax, **spines)
+ax.text(0, 0.7, s=r'$s$', ha='center', va='center')
 plot_rect(ax, square)
 ax.grid(False)
-ax.text(-1.5, 0, '$s$', verticalalignment="center", fontsize=14, color='k')
-
+fig.savefig("articulated_start.png")
 
 # %%
+
 
 def scale_xy(x=1, y=1):
     """scale the x and y axes"""
@@ -78,8 +82,8 @@ a, b, c, alpha, beta = 2, 3, 2, 30, 45
 gantry = trans_xy(y=a/2) @ scale_xy(y=a) @ square.copy()
 
 fig, ax = plt.subplots(1, 1)
-set_spines(ax, x_range=(-1, 6), y_range=(-1, 6))
-plot_rect(ax, gantry)
+set_spines(ax, **spines)
+plot_rect(ax, gantry, col=FILL1)
 ax.grid(False)
 
 
@@ -93,7 +97,8 @@ link1 = (trans_xy(y=a)
          @ square.copy())
 
 fig, ax = plt.subplots(1, 1)
-set_spines(ax, x_range=(-1, 6), y_range=(-1, 6))
+set_spines(ax, **spines)
+plot_rect(ax, gantry, col=FILL1)
 plot_rect(ax, link1, col=FILL2)
 ax.grid(False)
 
@@ -109,16 +114,54 @@ link2 = (trans_xy(y=a)
          @ square.copy())
 
 fig, ax = plt.subplots(1, 1)
-set_spines(ax, x_range=(-1, 6), y_range=(-1, 6))
+set_spines(ax, **spines)
+plot_rect(ax, gantry, col=FILL1)
+plot_rect(ax, link1, col=FILL2)
 plot_rect(ax, link2, col=FILL3)
 ax.grid(False)
 
 # %%
 
 fig, ax = plt.subplots(1, 1)
-set_spines(ax, x_range=(-1, 6), y_range=(-1, 6))
-plot_rect(ax, gantry)
+set_spines(ax, **spines)
+plot_rect(ax, gantry, col=FILL1)
 plot_rect(ax, link1, col=FILL2)
 plot_rect(ax, link2, col=FILL3)
 ax.grid(False)
+ax.text(-0.75, 1, s=rf'${a}s$', ha='center', va='center', fontsize=12)
+ax.text(1, 3.5, s=rf'${b}s$', ha='center', va='center', fontsize=12)
+ax.text(2, 4.75, s=rf'${c}s$', ha='center', va='center', fontsize=12)
+
+
+# %%
+
+p1_1 = (link1[:2, 0] + link1[:2, 3]) * 0.5
+p1_2 = p1_1 + 2 * (link1[:2, 1] - link1[:2, 0]).T
+
+p2_1 = (link2[:2, 0] + link2[:2, 3]) * 0.5
+p2_2 = p2_1 + 2 * (link2[:2, 1] - link2[:2, 0]).T
+
+fig, ax = plt.subplots(1, 1)
+set_spines(ax, **spines)
+plot_rect(ax, gantry, col=FILL1)
+plot_rect(ax, link1, col=FILL2)
+plot_rect(ax, link2, col=FILL3)
+ax.grid(False)
+ax.text(-0.75, 1, s=rf'${a}s$', ha='center', va='center', fontsize=12)
+ax.text(1, 3.5, s=rf'${b}s$', ha='center', va='center', fontsize=12)
+ax.text(2, 4.75, s=rf'${c}s$', ha='center', va='center', fontsize=12)
+ax.plot((0, 5), (2, 2), '--k', linewidth=1)
+ax.plot((p1_1[0], p1_2[0]), (p1_1[1], p1_2[1]), '--k', linewidth=1)
+ax.plot((p2_1[0], p2_2[0]), (p2_1[1], p2_2[1]), '--k', linewidth=1)
+
+AngleAnnotation(
+    p1_1, (p2_1[0], 2), p1_2, ax=ax, size=1000, unit='pixels',
+    textposition='outside',
+    color='k', text=rf"$\alpha={alpha}^{{\circ}}$")
+
+AngleAnnotation(
+    p2_1, p1_2, p2_2, ax=ax, size=600, unit='pixels',
+    textposition='outside',
+    color='k', text=rf"$\beta={beta}^{{\circ}}$")
+
 # %%
